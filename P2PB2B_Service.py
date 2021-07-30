2004281596595d946a67eca94efe2668ec5af6da
wxl201314 <49563111+wxl201314@users.noreply.github.com>
2021-07-30 16:51:06 +0800

    Add files via upload

---
+++ P2PB2B_Service.py
@@ -0,0 +1,67 @@
+from P2PB2BEX.p2pb2b_api import Api
+
+
+key = ''
+secret = ''
+p2p_api = Api(key,secret)
+class P2PB2B_API:
+	
+	#获取最新价
+	def get_last_price(self,symbol):
+		
+		data = p2p_api.public.ticker(market=symbol)
+		if data.get('success') == True:
+			return float(data.get('result').get('last'))
+		else:
+			return None
+	
+	#获取账户某币种可用余额
+	def get_balance(self,base):
+		data = p2p_api.account.balance(currency = base)
+		if data.get('success') == True:
+			return float(data.get('result').get('available'))
+		else:
+			return None
+	
+	#获取深度
+	def get_depth(self,symbol):
+
+		data = p2p_api.public.depth.result(market = symbol)
+		if data.get('success') == True:
+			return data.get('result')
+		else:
+			return None
+	
+	#获取未成交订单
+	def get_executed_history(self,symbol):
+		data = p2p_api.account.executed_history(market=symbol)
+		if data.get('success') == True:
+			return data.get('result')
+		else:
+			return None
+	
+	#创建订单
+	def create_order(self,symbol,price,amount,side):
+		data = p2p_api.order.new(market = symbol,price = price,amount = amount,side = side)
+		return data
+	
+	#取消订单
+	def cancel_order(self,symbol,orderid):
+		data = p2p_api.order.cancel(market = symbol,orderId = orderid)
+		return data
+	
+	#取消所有订单
+	def cancel_all_orders(self,symbol):
+		data = p2p_api.order.cancel.all(market=symbol)
+		if data.get('success') == True:
+			print('全部撤单成功')
+		else:
+			print('全部撤单失败')
+	
+	#获取订单成交历史
+	def get_order_history(self):
+		return p2p_api.account.order_history()
+
+if __name__ == "__main__":
+	q = P2PB2B_API()
+	print(q.get_balance('USDT'))
